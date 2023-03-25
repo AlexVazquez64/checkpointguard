4c268d17763fb933898286cbb9a4ac372ea297f7
Alejandro Vázquez <alejandrovazquezc@outlook.com>
2023-03-24 23:06:56 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.checkpointguard',
+  appName: 'checkpointguard',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
